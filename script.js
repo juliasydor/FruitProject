f57c7656a2146bc981be57149e7fce19b087ec5a
juliasydor <juliasydor.git@gmail.com>
2023-06-26 17:16:20 -0300

    update

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-let joquei = document.createElement('article');
 
-document.querySelector('body').appendChild(joquei);
+let title_1 = document.createElement('span')
+title_1.innerText = "Fruta"
 
-let tituilo = document.createElement('h1')
-let paragrafo = document.createElement('p')
-let linkk = document.createElement('a')
+let title_2 = document.createElement('h1')
+title_2.innerText = "Vida"
 
-joquei.appendChild(tituilo);
+let content = document.createElement('div')
+content.id = "divContent"
 
-joquei.appendChild(paragrafo);
+document.querySelector('body').appendChild(content)
 
-joquei.appendChild(linkk);
+content.appendChild(title_1)
+content.appendChild(title_2)
 
-tituilo.innerText = "Título do Post"
-paragrafo.innerText = "Descrição do Post"
-linkk.innerText = "Ver post Completo"
+
+let card_1 = document.createElement('div')
+card_1.innerText = "Banana"
+let card_2 = document.createElement('div')
+card_2.innerText = "Pera"
+let card_3 = document.createElement('div')
+card_3.innerText = "Manga"
+let card_4 = document.createElement('div')
+card_4.innerText = "Abacaxi"
+let card_5 = document.createElement('div')
+card_5.innerText = "Jabuticaba"
+
+
+let principal = document.createElement('article')
+document.querySelector('body').appendChild(principal)
+
+principal.appendChild(card_1)
+principal.appendChild(card_2)
+principal.appendChild(card_3)
+principal.appendChild(card_4)
+principal.appendChild(card_5)
+
+
+
+
+
+
 
 
 
