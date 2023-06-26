80ffbbc0652c290bcc589142365ef87bf8974ebe
juliasydor <juliasydor.git@gmail.com>
2023-06-26 16:34:32 -0300

    initil

---
+++ script.js
@@ -0,0 +1,28 @@
+let joquei = document.createElement('article');
+
+document.querySelector('body').appendChild(joquei);
+
+let tituilo = document.createElement('h1')
+let paragrafo = document.createElement('p')
+let linkk = document.createElement('a')
+
+joquei.appendChild(tituilo);
+
+joquei.appendChild(paragrafo);
+
+joquei.appendChild(linkk);
+
+tituilo.innerText = "Título do Post"
+paragrafo.innerText = "Descrição do Post"
+linkk.innerText = "Ver post Completo"
+
+
+
+
+
+
+
+
+
+
+
